02a631c322258a9ce8de7c5e8e6b71646b187920
Nikita Exaurov <urahaaragin@mail.ru>
2013-02-16 19:31:03 +0200

    Add time to comment

--- Lemon.WebApp/Models/OrderCommentViewModel.cs
+++ Lemon.WebApp/Models/OrderCommentViewModel.cs
@@ -2,6 +2,7 @@ namespace Lemon.WebApp.Models
 {
     using System;
 
+    using Lemon.Common;
     using Lemon.DataAccess.DomainModels;
 
     public class OrderCommentViewModel
@@ -10,10 +11,21 @@ namespace Lemon.WebApp.Models
         {
             this.Comment = comment.Comment;
             this.CreationTime = comment.CreatedTime;
+            Cost = comment.ProposedCost;
         }
 
         public string Comment { get; set; }
 
         public DateTime CreationTime { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public string CreationTimeOut
+        {
+            get
+            {
+                return DateTimeHelper.GetOutTime(CreationTime);
+            }
+        }
     }
 }
